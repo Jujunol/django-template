45280c389ca72d7592d9c183f81ae21069ed3b2a
Jujunol <johnallan4875@gmail.com>
2018-07-23 17:55:26 -0400

    Home page now renders package names used and a brief description

--- app/js/app.jsx
+++ app/js/app.jsx
@@ -10,7 +10,26 @@ class App extends Component {
     render() {
         return (
             <div className="container">
-                <h1>Content goes here</h1>
+                <div className="card-panel">
+                    <h1>Django App Template</h1>
+
+                    <h5>Installed Pip Packages</h5>
+                    <ul className="collection">
+                        <li className="collection-item">Django - Backend Framework</li>
+                        <li className="collection-item">Jinja2 - Templating Engine (better than Django's)</li>
+                        <li className="collection-item">Python-Decouple - .env var handler for Django</li>
+                    </ul>
+
+                    <h5>Installed Node Packages</h5>
+                    <ul className="collection">
+                        <li className="collection-item">Babel - There's a few of these to write nice clean code in Javascript</li>
+                        <li className="collection-item">React - For react apps</li>
+                        <li className="collection-item">Css-loader - interprets @import and url() like import/require() and will resolve them</li>
+                        <li className="collection-item">Sass-loader - converts sass and scss into plain css</li>
+                        <li className="collection-item">Node-sass - Also transpilation of scss and css to transpile faster</li>
+                        <li className="collection-item">Webpack - A build system used to build our assets</li>
+                    </ul>
+                </div>
             </div>
         );
     }
